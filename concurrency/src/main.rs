9d21396247f85ee09b4ce3ba6a8db0bd1f90b5c0
Antoine Latrille <antoine.latrille@gmail.com>
2019-05-13 19:45:32 +0200

    Add data ownership part

--- concurrency/src/main.rs
+++ concurrency/src/main.rs
@@ -2,6 +2,10 @@ use std::thread;
 use std::time::Duration;
 
 fn main() {
+    data_ownership();
+}
+
+fn concurrent() {
     let handle = thread::spawn(|| {
         for i in 1..10 {
             println!("Hi, number {} for thread", i);
@@ -14,5 +18,18 @@ fn main() {
         thread::sleep(Duration::from_millis(300));
     }
 
+    handle.join().unwrap();
+}
+
+fn data_ownership() {
+    let v = vec![1, 2, 3];
+    let handle = thread::spawn(move || {
+        for i in 1..3 {
+            println!("Hi, vec is {:?} for thread", v);
+            thread::sleep(Duration::from_millis(300));
+        }
+    });
+
+//    drop(v); oh noooo
     handle.join().unwrap();
 }
