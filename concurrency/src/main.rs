540dc94b98e2316b52e5d4f1dc0b1727cb47118c
Antoine Latrille <antoine.latrille@gmail.com>
2019-05-13 20:21:51 +0200

    Add mutex/arc part

--- concurrency/src/main.rs
+++ concurrency/src/main.rs
@@ -4,9 +4,28 @@
 use std::thread;
 use std::time::Duration;
 use std::sync::mpsc;
+use std::sync::{Mutex, Arc};
 
 fn main() {
-    channels();
+    mutex()
+}
+
+
+fn mutex() {
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+            *num += 1;
+        });
+        handles.push(handle);
+    }
+    for handle in handles {
+        handle.join().unwrap();
+    }
+    println!("Counter is  {:?}", *counter.lock().unwrap());
 }
 
 fn channels() {
